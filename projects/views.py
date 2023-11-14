12573e7377efa3fc256a0ba574abeb4709b0df5d
Dorothy Schwark <dorothy.schwark@gmail.com>
2023-11-13 19:11:10 -0600

    assg 3

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
 from .forms import ProjectForm
+from .models import Project
 
-def projects(request):
-    return render (request, 'projects/projects_list.html')
+def projects_list(request):
+    projects = Project.objects.all()
+    return render (request, 'projects/projects_list.html', {'projects' : projects})
 
 
 def project_new(request):
-        form = ProjectForm()
+        if request.method == 'POST':
+            form = ProjectForm(request.POST, request.FILES)
+            if form.is_valid():
+                project = form.save()
+                return redirect('projects_list')
+        else:
+            form = ProjectForm()
+
         return render(request, 'projects/project_new.html', {
         'form': form
     })
